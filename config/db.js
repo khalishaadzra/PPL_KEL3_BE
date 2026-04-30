ada50c9ad7e1a76c871ef9df374eef3978388936
Anisa Ramadhani <anisaramadhani585@gmail.com>
2026-04-30 16:29:50 +0700

    feat: fix connect db

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
-const mongoose = require("mongoose");
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
+    console.log('MongoDB connected');
   } catch (error) {
-    console.error("MongoDB connection failed", error);
-    process.exit(1);
+    console.error('MongoDB connection failed', error);
   }
 };
 
