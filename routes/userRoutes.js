3984a7193cd5f78760a27d5c08dde0fc85628ae2
Anisa Ramadhani <anisaramadhani585@gmail.com>
2026-05-04 21:33:38 +0700

    feat: Perbaiki BE Admin

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,10 @@
-
 const router = require("express").Router();
 const { registerUser, getUsers, getUserById, updateUser, changePassword } = require("../controllers/userController");
-const { deleteUser } = require("../controllers/deleteUserController");
-const { verifyAdmin } = require("../middleware/auth");
-
 
 router.post("/register", registerUser);
-router.get("/", verifyAdmin, getUsers);
-router.get("/:id", verifyAdmin, getUserById);
-router.put("/:id", verifyAdmin, updateUser);
+router.get("/", getUsers);
+router.get("/:id", getUserById);
+router.put("/:id", updateUser);
 router.post("/:id/change-password", changePassword);
-router.delete("/:id", verifyAdmin, deleteUser);
 
 module.exports = router;
