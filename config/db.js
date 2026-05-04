03c8504aac7ac74bb7b201e61d9abf4955bc9f8f
khalishaadzra <khalishaadzraini@gmail.com>
2026-05-05 02:00:51 +0700

    tambah catch

--- config/db.js
+++ config/db.js
@@ -5,9 +5,10 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error('MongoDB connection failed', error);
+    console.error("MongoDB connection failed", error);
+    process.exit(1);
   }
 };
 
